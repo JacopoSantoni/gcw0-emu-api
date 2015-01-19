b0d28f01a76ffaf681b78e4817e9b89ed5c6b23f
Jack <jack@pixbits.com>
2015-01-19 16:38:14 +0100

    added serialization for settings

--- src/data/serializer.h
+++ src/data/serializer.h
@@ -2,6 +2,7 @@
 #define __SERIALIZER_H__
 
 #include <vector>
+#include <unordered_map>
 
 #include "../common/utils.h"
 #include "../systems/systems.h"
@@ -19,6 +20,12 @@ using namespace std;
 class Serializer
 {
 public:
+  template<typename Writer> void serialize(const char* key, bool value, Writer& writer) const { writer.String(key); writer.Bool(value); }
+  template<typename Writer> void serialize(const char* key, const char* value, Writer& writer) const { writer.String(key); writer.String(value); }
+  template<typename Writer> void serialize(const char* key, u32 value, Writer& writer) const { writer.String(key); writer.Uint(value); }
+  template<typename Writer> void serialize(const char* key, const std::string& value, Writer& writer) const { writer.String(key); writer.String(value.c_str()); }
+
+  
   template<typename T, typename Writer>
   void serialize(const std::vector<T>& data, Writer& writer, const char* name = nullptr) const
   {
@@ -29,6 +36,21 @@ public:
     for (const auto& element : data)
       serialize(element, writer);
     writer.EndArray();
+  }
+  
+  template<typename V, typename Writer>
+  void serialize(const std::unordered_map<std::string,V>& data, Writer& writer, const char* name = nullptr) const
+  {
+    if (name)
+      writer.String(name);
+    
+    writer.StartObject();
+    for (const auto& element : data)
+    {
+      serialize(element.first, writer);
+      serialize(element.second, writer);
+    }
+    writer.EndObject();
   }
   
   template<typename T, typename Writer>
@@ -67,6 +89,7 @@ public:
     serialize(info.details, writer, "details");
     serialize(info.supportedButtons(), writer, "buttons");
     serialize(info.supportedScalers(), writer, "scalers");
+    serialize(info.supportedSettings(), writer, "settings");
     serialize(info.gfxSpec, writer, "gfx");
   }
   
@@ -82,14 +105,10 @@ public:
   template<typename Writer>
   void serialize(const CoreDetails& details, Writer& writer) const
   {
-    writer.String("name");
-    serialize(details.name, writer);
-    writer.String("author");
-    serialize(details.author, writer);
-    writer.String("porter");
-    serialize(details.porter, writer);
-    writer.String("webpage");
-    serialize(details.webpage, writer);
+    serialize("name", details.name, writer);
+    serialize("author", details.author, writer);
+    serialize("porter", details.porter, writer);
+    serialize("webpage", details.webpage, writer);
   }
   
   template<typename Writer>
@@ -97,15 +116,47 @@ public:
   {
     writer.String("scaler");
     serialize(preferences.scaler, writer);
+    serialize(preferences.settings, writer, "settings");
+  }
+  
+  template<typename Writer>
+  void serialize(const Setting& setting, Writer& writer) const
+  {
+    const char* type = nullptr;
+    const char* group = nullptr;
+    
+    switch (setting.type) {
+      case Setting::Type::BOOLEAN: type = "boolean"; break;
+      case Setting::Type::ENUMERATION: type = "enumeration"; break;
+      case Setting::Type::PATH: type = "path"; break;
+    }
+    
+    switch (setting.group) {
+      case Setting::Group::VIDEO: group = "video"; break;
+      case Setting::Group::AUDIO: group = "audio"; break;
+      case Setting::Group::MISC: group = "misc"; break;
+    }
+    
+    writer.StartObject();
+    serialize("type", type, writer);
+    serialize("group", group, writer);
+    serialize("ident", setting.ident, writer);
+    serialize("name", setting.name, writer);
+    serialize("default", setting.defaultValue, writer);
+    serialize("canBeModifiedAtRuntime", setting.canBeModifiedAtRuntime, writer);
+    
+    if (setting.type == Setting::Type::ENUMERATION)
+      serialize(setting.values, writer, "values");
+    
+    writer.EndObject();
   }
   
   template<typename Writer>
   void serialize(const GfxBufferSpec& gfxSpec, Writer& writer) const
   {
-    writer.String("width");
-    writer.Uint(gfxSpec.width);
-    writer.String("height");
-    writer.Uint(gfxSpec.height);
+    serialize("width", (u32)gfxSpec.width, writer);
+    serialize("height", (u32)gfxSpec.height, writer);
+    
     writer.String("format");
     
     const char* formatStr = nullptr;
@@ -118,26 +169,19 @@ public:
       case FORMAT_XRGB888: formatStr = "XRGB888"; break;
     }
     
-    writer.String(formatStr);
+    serialize("format", formatStr, writer);
   }
   
   template<typename Writer>
   void serialize(const ButtonSetting& button, Writer& writer) const
   {
     writer.StartObject();
-    writer.String("name");
-    serialize(button.getName(), writer);
-    writer.String("shiftAmount");
-    writer.Uint(button.getShiftAmount());
-    writer.String("mask");
-    writer.Uint(button.getMask());
-    writer.String("defaultMask");
-    writer.Uint(button.getDefaultMask());
-    writer.String("isRebindable");
-    writer.Bool(button.isRebindable());
-    writer.String("canBeMultikey");
-    writer.Bool(button.canBeMultikey());
-    
+    serialize("name", button.getName(), writer);
+    serialize("shiftAmount", (u32)button.getShiftAmount(), writer);
+    serialize("mask", button.getMask(), writer);
+    serialize("defaultMask", button.getDefaultMask(), writer);
+    serialize("isRebindable", button.isRebindable(), writer);
+    serialize("canBeMultikey", button.canBeMultikey(), writer);
     writer.EndObject();
   }
   
@@ -164,9 +208,16 @@ public:
   }
   
   template<typename T>
+  void unserialize(std::unordered_map<std::string,T>& data, const Value& value)
+  {
+    for (auto it = value.MemberBegin(); it != value.MemberEnd(); ++it)
+      data.insert(std::make_pair(it->name.GetString(), it->value.GetString()));
+  }
+  
+  template<typename T>
   T unserialize(const Value& value)
   {
-    
+    static_assert(sizeof(T) != sizeof(T), "Unserializer must be specialized");
   }
   
 
@@ -189,6 +240,35 @@ ButtonSetting Unserializer::unserialize(const Value& value)
 }
 
 template<>
+Setting Unserializer::unserialize(const Value& value)
+{
+  std::string type = value["type"].GetString();
+  std::string group = value["group"].GetString();
+
+  std::string ident = value["ident"].GetString();
+  std::string name = value["name"].GetString();
+  std::string defaultValue = value["default"].GetString();
+  bool canBeModifiedAtRuntime = value["canBeModifiedAtRuntime"].GetBool();
+ 
+  Setting::Group egroup;
+  
+  if (group == "video") egroup = Setting::Group::VIDEO;
+  else if (group == "audio") egroup = Setting::Group::AUDIO;
+  else if (group == "misc") egroup = Setting::Group::MISC;
+  
+  if (type == "boolean")
+    return Setting(ident, name, egroup, defaultValue == "true" ? true : false, canBeModifiedAtRuntime);
+  else if (type == "path")
+    return Setting(ident, name, egroup, defaultValue, canBeModifiedAtRuntime);
+  else
+  {
+    std::vector<std::string> values;
+    unserialize(values, value["values"]);
+    return Setting(ident, name, egroup, values, defaultValue, canBeModifiedAtRuntime);
+  }
+}
+
+template<>
 CoreDetails Unserializer::unserialize(const Value& value)
 {
   std::string name = value["name"].GetString();
@@ -207,6 +287,7 @@ CorePreferences Unserializer::unserialize(const Value& value)
   CorePreferences preferences;
   
   preferences.scaler = scaler;
+  unserialize(preferences.settings, value["settings"]);
   
   return preferences;
 }
@@ -256,6 +337,10 @@ CoreInfo Unserializer::unserialize(const Value& value)
   std::vector<std::string> scalers;
   unserialize(scalers, value["scalers"]);
   info.setScalers(scalers);
+  
+  std::vector<Setting> settings;
+  unserialize(settings, value["settings"]);
+  info.setSettings(settings);
   
   info.timestamp = value["timestamp"].GetUint();
   info.ident = unserialize<CoreIdentifier>(value["ident"]);
